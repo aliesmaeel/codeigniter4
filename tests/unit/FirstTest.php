1ea2dfd68fb17cf084179f4c52ad0b207784cd8f
Ali Esmaeel <esmaeel@mobilapp.io>
2024-10-17 18:36:40 +0200

    ajax request for delete

---
+++ tests/unit/FirstTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace unit;
+
+use CodeIgniter\Test\CIUnitTestCase;
+
+class FirstTest extends CIUnitTestCase
+{
+
+    public function testMultiplicationOfTwoNumbers(){
+        $this->assertTrue(true);
+    }
+}
